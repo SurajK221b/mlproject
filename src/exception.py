4409a1ba5d725c824f8d34f6c96c478a77ab3a3b
SurajK221b <suraj.khodade7@gmail.com>
2025-07-22 19:13:47 +0530

    data ingestion completed

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-import logger  # This imports the logger module which configures logging
+from . import logger  # This imports the logger module which configures logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
